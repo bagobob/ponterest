8e6bc016e37628a53bd3e2abbbe2950d7ff74edc
anaelle.jeagni <anaellejeagni@gmail.com>
2020-12-20 16:25:09 +0100

    Explicit
    Explicit is better than implcit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ponterest
 symfony web application
 Clone of Pinterest to learn symfony
+
+composer update
+yarn install
+yarn add --dev @symfony/webpack-encore
+yarn add webpack-notifier --dev
+yarn encore dev
+Mailtrap
--- src/Form/PinFormType.php
+++ src/Form/PinFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Pin;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -18,12 +20,16 @@ class PinFormType extends AbstractType
                 'label'    =>'Image (JPG or PNG file)',
                 'required' => false,
                 'allow_delete' => true,
+                'download_uri' => false,
                 'imagine_pattern' => 'squared_thumbnail_small'
             ])
-            ->add('title')
-            ->add('description')
+            ->add('title',TextType::class)
+            ->add('description',TextareaType::class)
             ->add('createdAt', DateTimeType::class, [
                 'data' => new \DateTime(),
+                'attr' => [
+                    'class' => 'd-print-none'
+                ]
             ])
             ->add('updatedAt', DateTimeType::class,[
                 'data' => new \DateTime(),
